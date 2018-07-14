37c1bad6303a03103311f3a8ee54bfeffa1a1bfb
ArminBlaze <box_for_spam@mail.ru>
2018-07-14 20:05:47 +0300

    Color-module callback

--- js/color.js
+++ js/color.js
@@ -10,22 +10,47 @@
 	function onWizardClick (e) {
 		var target = e.target;
 		console.log(target);
+		var color;
+		var callback;
 
 		if(target.classList.contains('wizard-coat')) {
-			var color = generateRandomParam("coatColors");
-			console.log(color);
+//			var color = generateRandomParam("coatColors");
+//			console.log(color);
+			color = ['red', 'green', 'blue'];
+			callback = changeFill;
 		} else if (target.classList.contains('wizard-eyes')) {
-			var color = generateRandomParam("eyesColors");
-			console.log(color);
+//			var color = generateRandomParam("eyesColors");
+//			console.log(color);
+			color = ['navy', 'teal', 'orange'];
+			callback = changeFill;
 		} else if (target.classList.contains('setup-fireball')) {
-			var color = generateRandomParam("fireballsColors");
-			console.log(color);
-			target.parentNode.style.backgroundColor = color;
-
-			return;
+//			var color = generateRandomParam("fireballsColors");
+//			console.log(color);
+//			target.parentNode.style.backgroundColor = color;
+			target = target.parentNode;
+			color = ['yellow', 'black', 'aliceblue'];
+			callback = changeBackground;
+//			return;
 		}
 
-		target.style.fill = color;
+//		target.style.fill = color;
+		colorizeElement(target, color, callback);
+	}
+	
+	function changeFill (elem, color) {
+		elem.style.fill = color;
+	}
+	
+	function changeBackground (elem, color) {
+		elem.style.backgroundColor = color;
+	}
+	
+	function colorizeElement (elem, colors, callback) {
+		if(arguments.length < 3) return;
+		
+		var color = window.util.pickRandomFromArr( colors );
+		
+		callback(elem, color);
 	}
 
 	function generateRandomParam(prop) {
