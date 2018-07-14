40c0aaf2d8c8b6d5ad9de0212f22444413f473d5
ArminBlaze <box_for_spam@mail.ru>
2018-07-14 14:28:52 +0300

    05 - DnD removed comments

--- js/dialog.js
+++ js/dialog.js
@@ -71,7 +71,7 @@
 	
 	function onUserpicMousedown (e) {
 		e.preventDefault();
-		var coords = setup.getBoundingClientRect();
+		var coords = getCoords(setup);
 		console.log(coords);
 		
 		var shiftX = e.pageX - coords.left;
@@ -96,18 +96,7 @@
 			setup.style.top = e.pageY - shiftY + 'px';
 		}
 		
-//		 3, перемещать по экрану
-//		document.onmousemove = function(e) {
-//			moveAt(e);
-//		};
-		
 		document.addEventListener('mousemove', onMouseMove);
-
-		// 4. отследить окончание переноса
-//		document.onmouseup = function() {
-//			document.onmousemove = null;
-//			document.onmouseup = null;
-//		};
 		
 		document.addEventListener('mouseup', onMouseUp);
 		
@@ -119,57 +108,14 @@
 		popupIcon.ondragstart = function() {
 			return false;
 		};
+		
+		function getCoords(elem) {   // кроме IE8-
+			var box = elem.getBoundingClientRect();
+			return {
+				top: box.top + pageYOffset,
+				left: box.left + pageXOffset
+			};
+		}
 	}
 	
-// ball.onmousedown = function(e) { // 1. отследить нажатие
-//
-//		var coords = getCoords(ball);
-//		var shiftX = e.pageX - coords.left;
-//		var shiftY = e.pageY - coords.top;
-//
-//		// подготовить к перемещению
-//		// 2. разместить на том же месте, но в абсолютных координатах
-//		ball.style.position = 'absolute';
-//		moveAt(e);
-//
-//		// переместим в body, чтобы мяч был точно не внутри position:relative
-//		document.body.appendChild(ball);
-//
-//		ball.style.zIndex = 1000; // показывать мяч над другими элементами
-//
-//		// передвинуть мяч под координаты курсора
-//		// и сдвинуть на половину ширины/высоты для центрирования
-//		function moveAt(e) {
-//			ball.style.left = e.pageX - shiftX + 'px';
-//			ball.style.top = e.pageY - shiftY + 'px';
-//		}
-//
-//		// 3, перемещать по экрану
-//		document.onmousemove = function(e) {
-//			moveAt(e);
-//		};
-//
-//		// 4. отследить окончание переноса
-//		document.onmouseup = function() {
-//			document.onmousemove = null;
-//			document.onmouseup = null;
-//		};
-//
-//
-//
-//		//вспомогательная ф-ция получения координат с прокруткой
-//		function getCoords(elem) {   // кроме IE8-
-//			var box = elem.getBoundingClientRect();
-//			return {
-//				top: box.top + pageYOffset,
-//				left: box.left + pageXOffset
-//			};
-//		}
-//	}
-//
-//
-//	//отмена перетаскивания HTML5 dragstart
-//	ball.ondragstart = function() {
-//		return false;
-//	};
 })();
